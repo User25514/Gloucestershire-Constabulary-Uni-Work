e51a80ab0ff1338b3c1ebf66ce0da223df91c950
Yen <62843535+User25514@users.noreply.github.com>
2022-02-12 06:06:20 +0000

    Place holders and password cant go above 16 characters

--- KeyPressCapture.js
+++ KeyPressCapture.js
@@ -1,4 +1,3 @@
-
 document.getElementById("LoginEmail").onfocus = KeyboardCapture("LoginEmail");
 document.getElementById("LoginPassword").onfocus = KeyboardCapture("LoginPassword");
 document.getElementById("RegisterFirstName").onfocus = KeyboardCapture("RegisterFirstName");
@@ -7,25 +6,36 @@ document.getElementById("RegisterPhoneNumber").onfocus = KeyboardCapture("Regist
 document.getElementById("RegisterEmail").onfocus = KeyboardCapture("RegisterEmail");
 document.getElementById("RegisterPassword").onfocus = KeyboardCapture("RegisterPassword");
 document.getElementById("RegisterConfirmPassword").onfocus = KeyboardCapture("RegisterConfirmPassword");
+
 function KeyboardCapture(Location) {
+    console.log("start Keyboard");
     const element = document.querySelector(`#${Location}`);
-    document.addEventListener("keydown",logKey);
-    
-    console.log(document.getElementById(`${Location}`).value);
+    document.addEventListener("keydown", logKey);
     let Text = document.getElementById(`${Location}`).value;
-    const EmailString = Text.split("");
+    const TextString = Text.split("");
+
     function logKey(e) {
         if (element === document.activeElement) {
-        const Key = e.key;
-        if (e.key === "Backspace") {
-            EmailString.pop();
-        } else if ((e.key).length > 1){
-        } else{
-            EmailString.push(e.key);
-        }
-        document.getElementById(`${Location}`).value =EmailString.join("");
-        }else{
-            document.removeEventListener("keypress",logKey);
+            if (e.key === "Backspace") {
+                TextString.pop();
+            } else if ((e.key).length > 1) {} else {
+                if (Location == "LoginPassword" || Location == "RegisterPassword" || Location == "RegisterConfirmPassword") {
+                    if (TextString.length < 16) {
+                        TextString.push(e.key);
+                    } else if (TextString.length == 16) {
+                        alert("You cannot have more than 16 characters.");
+                    }
+                } else {
+
+                    TextString.push(e.key);
+                    console.log(Location);
+                    console.log(e.key);
+                }
+            }
+            document.getElementById(`${Location}`).value = TextString.join("");
+        } else {
+            console.log("Kill");
+            document.removeEventListener("keypress", logKey);
             return;
         }
     }
