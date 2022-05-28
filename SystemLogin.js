588df5040387fb8cdfc2522d2658d7afe891e3b2
Yen <62843535+User25514@users.noreply.github.com>
2022-05-28 02:07:59 +0100

    Error Label Updates
    Error labels are not alerts anymore and show up as red text instead underneath the text boxes.

--- SystemLogin.js
+++ SystemLogin.js
@@ -5,14 +5,14 @@ function SystemLogin() {
     };
     if (PageInfo.Email != "" && PageInfo.Email.includes("@" && ".") && PageInfo.Email.includes(".com" || ".co.uk")) {
         if (PageInfo.Password != "" && PageInfo.Password.length >= 8 && PageInfo.Password.length <= 16 && PageInfo.Password.match(/[a-z]/i) && PageInfo.Password.match(/[A-Z]/i) && PageInfo.Password.match(/[0-9]/i) && PageInfo.Password.match(/[!@#$%^&*]/i)) {
-            alert("Login.");
+            //alert("Login.");
             return true;
         } else {
-            alert("Your password must be between 8 and 16 characters long, contain at least one lowercase letter, one uppercase letter, one number and one special character.");
+            document.getElementById("LoginPasswordError").innerHTML = "Invalid Password";
             return false;
         }
     } else {
-        alert("Use a valid email address.");
+        document.getElementById("LoginEmailError").innerHTML = "Invalid email address.";
         return false;
     }
 }
