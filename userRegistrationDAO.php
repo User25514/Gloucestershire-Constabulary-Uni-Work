3f7620de0df9c3adce60866a60aa1941ab5f9e71
Yen <62843535+User25514@users.noreply.github.com>
2022-05-28 02:37:51 +0100

    PHP BugHunting

--- userRegistrationDAO.php
+++ userRegistrationDAO.php
@@ -1,6 +1,9 @@
 <?php
+    echo "Start";
     if($_POST['phpFunction'] == 'create')
+        echo "Lets Go";
         create();
+        
     else if ($_POST['phpFunction'] == 'retrieve')
         retrieve();
     else if ($_POST['phpFunction'] == 'update')
@@ -8,14 +11,16 @@
     else if ($_POST['phpFunction'] == 'delete')
         delete();
     function create() {
+        echo "Start Function";
         $firstname = $_POST['firstName'];
         $lastname = $_POST['lastName'];
         $dob = $_POST['dateofbirth'];
         $phonenumber = $_POST['phonenumber'];
         $email = $_POST['email'];
         $pass = $_POST['password'];
+        echo "Done Variables";
         include "config.php";
-        $sql = "INSERT INTO `tbl_user`"." values "."('','$firstname','$lastname','$dob','$phonenumber','$email','$pass')";
+        $sql = "INSERT INTO `tbl_user`"." values "."('$firstname','$lastname','$dob','$phonenumber','$email','$pass')";
         if(mysqli_query($connection,$sql)) {
             echo "Successfully Registered.";
         } else {
